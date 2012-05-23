1f9fb228fb723a013faf165ba93aad65c4541518
Jonas Krückel <osm@jonas-krueckel.de>
2012-05-23 22:59:27 +0200

    remove responsive elements in nav bar, not necessary right now

--- index.php
+++ index.php
@@ -35,19 +35,12 @@
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </a>
           <a class="brand" href="#">Taarifa</a>
-          <div class="nav-collapse">
             <ul class="nav">
               <li class="active"><a href="#">Home</a></li>
               <li><a href="https://github.com/taarifa/Taarifa_Web">GitHub</a></li>
               <li><a href="http://taarifa.wordpress.com/">Blog</a></li>
             </ul>
-          </div><!--/.nav-collapse -->
         </div>
       </div>
     </div>
